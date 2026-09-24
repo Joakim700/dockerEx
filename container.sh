35db713900fc3b431350835fd24b0c05650ed933
Student <student@teknologi2.local>
2026-09-24 10:54:56 +0200

    log4try

--- container.sh
+++ container.sh
@@ -2,9 +2,12 @@
 set -e
 
 CONATINER_NAME="hello-world"
+CONTAINER_NAME="web"
+
 docker run --name "hello-world" -d nginx
 docker exec hello-world ls /usr/share/nginx/html
 docker logs hello-world
 
-docker exec "web" ls /user/share/nginx/html
+docker run --name "web" -d nginx
+docker exec web ls /user/share/nginx/html
 docker logs web
