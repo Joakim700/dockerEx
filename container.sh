c26f91bb6e57f284ee63c25ee95659084fb2f31a
Joakim700 <132199224+Joakim700@users.noreply.github.com>
2026-09-24 10:39:04 +0200

    Fix container name typo and update docker command

--- container.sh
+++ container.sh
@@ -1,6 +1,6 @@
 
 set -e
 
-CONATINER_NAME="hello_world"
-docker run --name "hello_world" -d nginx
+CONATINER_NAME="hello-world"
+docker run --name "hello-world" -d nginx
 docker exec hello-world ls /usr/share/nginx/html
