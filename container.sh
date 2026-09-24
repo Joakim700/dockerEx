12670397846cc1c7149ba796d6e980e570a26009
Student <student@teknologi2.local>
2026-09-24 10:51:00 +0200

    logs2

--- container.sh
+++ container.sh
@@ -6,3 +6,5 @@ docker run --name "hello-world" -d nginx
 docker exec hello-world ls /usr/share/nginx/html
 docker logs hello-world
 
+docker exec "hello-world2" ls /user/share/nginx/html
+docker logs hellow-world2
